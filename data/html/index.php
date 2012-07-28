d7ee836fdbc76b0efa8f73c3e76b44d409dc99c1
Alex Bowers <bowersbros@me.com>
2012-07-28 16:24:25 +0100

    Templates for Front-End

---
+++ data/html/index.php
@@ -0,0 +1,28 @@
+<?php
+	$root = "";
+	require($root . 'lib/php/functions.php');
+    require($root . 'lib/php/header.php');
+?>
+            <div class="container">
+                <div class="hero-unit">
+                   <h2><?=$config['website_name'];?></h2>
+                   <hr />
+                   <p>We're Different From other file lockers.</p>
+                   <p>We don't allow infringement trolls. We let you fight your case against takedown requests.</p>
+                   <hr />
+                   <p>
+                     <a href="better.php" class="btn btn-primary btn-large">
+                       Find Out More
+                     </a>
+                     <a href="account/login.php" class="btn btn-large">
+                     	Login / Register
+                     </a>
+                   </p>
+                </div>
+            </div>
+          </body>
+            <script>
+				$('.dropdown-toggle').dropdown();
+            </script>
+    </body>
+</html>
