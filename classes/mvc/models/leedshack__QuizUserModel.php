8abefd2a97f5a27e72ebff80cbc4d6ccbe9987fc
Alex Bowers <bowersbros@me.com>
2012-07-28 16:00:42 +0100

    Added QuizUserModel

---
+++ classes/mvc/models/leedshack__QuizUserModel.php
@@ -0,0 +1,29 @@
+<?php
+/* START AUTO */
+class leedshack__QuizUserModel extends leedshack__BaseModel {
+	protected static $table = 'quizuser';
+
+	protected $id;
+	protected $quizId;
+	protected $userId;
+	
+	public static function write($db, $object) {
+		$db->insertOrUpdateOne(
+			static::$table,
+			'id = %i', $object->getId(),
+			'quizid = %i', $object->getQuizId(),
+			'userid = %i', $object->getUserId()
+		);
+	}
+
+	protected static function loadFromSqlRow($row) {
+		$object = new self;
+
+		$object->setId($row->i_id);
+		$object->setUserId($row->i_userid);
+		$object->setQuizId($row->i_quizid);
+
+		return $object;
+	}
+}
+?>
