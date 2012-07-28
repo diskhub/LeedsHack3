1482e708b2284439e9aeaa3e431f52d3bdc5dc2d
Jonny Stirling <phoenix@jonstirling.co.uk>
2012-07-28 16:02:02 +0200

    Basic model + testing

--- classes/mvc/controllers/leedshack__smsController.php
+++ classes/mvc/controllers/leedshack__smsController.php
@@ -1,6 +1,7 @@
 <?php
 class leedshack__smsController extends leedshack__AbstractController {
 	function page_inbound() {
+		$data = leedshack__QuizMasterModel::loadAll($this->app->init_db);
 		var_dump("test");
 		exit;
 	}
---
+++ classes/mvc/models/leedshack__QuizMasterModel.php
@@ -0,0 +1,49 @@
+<?php
+/* START AUTO */
+class leedshack__QuizMasterModel extends leedshack__BaseModel {
+	protected static $table = 'quizmaster';
+
+	protected $id;
+	protected $email;
+	protected $password;
+	
+	public static function write($db, $object) {
+		$db->insertOrUpdateOne(
+			static::$table,
+			'id = %i', $object->getId(),
+			'email = %s', $object->getEmail()
+		);
+	}
+
+	protected static function loadFromSqlRow($row) {
+		$object = new self;
+
+		$object->setId($row->i_id);
+		$object->setEmail($row->s_email);
+
+		return $object;
+	}
+	
+/* FINISH AUTO */
+
+	public static function loadById($db, $id) {
+		$row = $db->fetchOne('*', static::$table, 'id = %i', (string)$id);
+		if(!$row) {
+			throw new Exception('Invalid ID');
+		}
+		return static::loadFromSqlRow($row);
+	}
+
+	public static function loadAll($db) {
+		$rows = $db->fetch('*', static::$table);
+
+		$users = array();
+		foreach($rows as $row) {
+			$users[] = static::loadFromSqlRow($db, $row);
+		}
+
+		return $users;
+	}
+
+}
+?>
